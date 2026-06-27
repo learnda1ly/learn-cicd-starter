47568de4f8c7e1422401ecf1d6358642ecb60831
learnda1ly <stephen.a.quinlan@gmail.com>
2026-06-26 19:40:13 -0500

    added failing test to ci.yml

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("Authorization", "ApiKey 1234567890")
+
+	got, _ := GetAPIKey(headers)
+	// want := "1234567890"
+	want := "1234567899"
+
+	if got != want {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
